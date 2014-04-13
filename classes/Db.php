bb89ab3e0c374ee46c5a3179d0bb9232fc1a01f6
F. Stephen Kirschbaum <fskirschbaum@gmail.com>
2014-04-13 11:35:05 -0500

    Begin building app structure for demostrating sCRUD

--- classes/Db.php
+++ classes/Db.php
@@ -55,7 +55,7 @@ class Db
 	*/
   private function Connect()
   {
-    $this->settings = parse_ini_file( "db.settings.ini.local.php" );
+    $this->settings = parse_ini_file( "db.settings.ini.remote.php" );
     $mysql = 'mysql:dbname='.$this->settings["dbname"].';host='.$this->settings["host"].'';
     try 
     {
